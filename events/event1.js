4b546aa498b0a340e0d5c5fcec2beec54d06b5ba
Albom-Sobytiy <natalyacorzhenko@yandex.ru>
2026-09-07 16:29:31 +0300

    Update image paths for Лига Авантюристов

--- events/event1.js
+++ events/event1.js
@@ -2,21 +2,35 @@ window.events.push({
     "id": 1,
     "название": "Лига Авантюристов",
     "тип": "на время",
-    "скриншоты": ["Лиг авантюр заставка 88.png"],   // <— заменено
-    "фон": "https://raw.githubusercontent.com/Albom-Sobytiy/album-events/main/%D0%9B%D0%B8%D0%B3%D0%B0%20%D0%90%D0%B2%20%D1%84%D0%BE%D0%BD.png",
+    "скриншоты": ["liga-avantyuristov/Лиг авантюр заставка 88.png"],
+    "фон": "liga-avantyuristov/Лига Ав фон.png",
     "награды": [
         {
             "название": "НОВИЧОК",
-            "фото": ["novichok1.jpg", "Л А новичок - 2.jpg", "Л А новичок - 3.jpg"]
+            "фото": [
+                "liga-avantyuristov/Л А новичок -1.jpg",
+                "liga-avantyuristov/Л А новичок - 2.jpg",
+                "liga-avantyuristov/Л А новичок - 3.jpg"
+            ]
         },
         {
             "название": "ОПЫТНЫЙ",
-            "фото": ["Л А опытный - 1.jpg", "Л А опытный - 2.jpg", "Л А опытный - 3.jpg", "Л А опытный - 4.jpg"]
+            "фото": [
+                "liga-avantyuristov/Л А опытный - 1.jpg",
+                "liga-avantyuristov/Л А опытный - 2.jpg",
+                "liga-avantyuristov/Л А опытный - 3.jpg",
+                "liga-avantyuristov/Л А опытный - 4.jpg"
+            ]
         },
         {
             "название": "МАСТЕР",
-            "фото": ["Л А мастер - 1.jpg", "Л А мастер - 2.jpg", "Л А мастер - 3.jpg", "master4.jpg"]
+            "фото": [
+                "liga-avantyuristov/Л А мастер - 1.jpg",
+                "liga-avantyuristov/Л А мастер - 2.jpg",
+                "liga-avantyuristov/Л А мастер - 3.jpg",
+                "liga-avantyuristov/Л А мастер - 4.jpg"
+            ]
         }
     ],
-    "боссы": "https://raw.githubusercontent.com/Albom-Sobytiy/album-events/main/result_БОССЫ%20Лига%20Авантюристов.jpeg"
+    "боссы": "liga-avantyuristov/result_БОССЫ Лига Авантюристов.jpeg"
 });
